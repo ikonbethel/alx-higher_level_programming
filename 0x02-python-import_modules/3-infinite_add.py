fcba5a8c2dd4f41d8571796adfed88f417de42f0
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2023-01-06 15:53:09 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,11 @@
-
 #!/usr/bin/python3
-
-if __name__ == "__main__":
-    """Print the addition of all arguments."""
-    import sys
-
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+from sys import argv
+if __name__ == '__main__':
+    if len(argv) == 1:
+        print("0")
+    else:
+        counter = 0
+        for i in range(1, len(argv)):
+            tmp = int(argv[i])
+            counter += tmp
+        print("{}".format(counter))
