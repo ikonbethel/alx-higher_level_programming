e6110c0d74e8598ada39a6879883fd19778267e0
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2023-04-29 06:12:13 +0100

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-# Star wars API
-import requests
+"""This will ist the 10 most recent commits on a given GitHub repo.
+Usage: ./100-github_commits.py <repository name> <repository owner>
+"""
 import sys
+import requests
 
 
 if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    repo = sys.argv[1]
-    owner = sys.argv[2]
-    result = requests.get("https://api.github.com/repos/{}/{}/commits".
-                          format(owner, repo))
-
-    dict_json = result.json()
-    if len(dict_json) > 10:
-        n = 10
-    else:
-        n = len(dict_json)
-    for j in range(n):
-        i = dict_json[j]
-        print("{}: {}".format(i.get('sha'), i.get('commit').
-              get('author').get('name')))
+    req = requests.get(url)
+    my_commits = req.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                my_commits[i].get("sha"),
+                my_commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
