96957749d74b1623e96cdb741702524983ccd72b
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2023-08-15 06:01:37 +0100

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// This program prints C is fun, Python is cool, Javascript is amazing
+// This programs prints C is fun, Python is cool, Javascript is amazing
 console.log('C is fun');
 console.log('Python is cool');
 console.log('Javascript is amazing');
