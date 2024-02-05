23e4b708a4ede9235d37c53df72137d958e7d322
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2024-02-06 00:54:41 +0100

    Update 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-
 import sys
+
 
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except Exception as ex:
-        sys.stderr.write("Exception: ")
-        sys.stderr.write(ex.args[0])
-        sys.stderr.write("\n")
+    except Exception as e:
+        sys.stderr.write(f"Exception: {str(e)}\n")
         return False
