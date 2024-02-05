6d61ee418db043d2f581695fde3a8ea8ade8c2d6
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2024-02-06 00:50:59 +0100

    Update 101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
 import sys
+
 
 def safe_function(fct, *args):
     try:
-        return fct(*args)
-    except Exception as ex:
-        sys.stderr.write("Exception: ")
-        sys.stderr.write(ex.args[0])
-        sys.stderr.write("\n")
+        result = fct(*args)
+        return result
+    except Exception as e:
+        sys.stderr.write("Exception: {}\n".format(e))
         return None
