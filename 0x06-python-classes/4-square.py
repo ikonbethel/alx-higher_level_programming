d57030ee2b750ef60cf63f0cb11ad819477839da
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2024-02-07 00:00:24 +0100

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
+"""Defines a square"""
 
-""" creates class Square """
 
 class Square:
-    """ Square class"""
+    """ An empty class that defines a Square
 
+    Attributes:
+        __size (int): Size of square. Size must be at least 0
+    """
     def __init__(self, size=0):
-        if type(size) != int:
+        """init method
+
+        Args:
+            size (int): Size of Square
+        """
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
@@ -15,11 +23,13 @@ class Square:
 
     @property
     def size(self):
+        """ Returns the size of square
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
@@ -27,4 +37,12 @@ class Square:
             self.__size = value
 
     def area(self):
-        return self.__size * self.__size         
+        """ Calculates the area of square
+
+        Args:
+            self: Instance of square
+
+        Return:
+            Area of square size
+        """
+        return (self.__size * self.__size)
