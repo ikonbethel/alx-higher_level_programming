b7132feb734863194bd8a559021b78c7cdb03735
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2024-02-21 02:50:59 +0100

    Update 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""This module defines a JSON file-reading function"""
+"""
+Module containing function that creates an Object from a “JSON file”
+"""
 import json
 
 
 def load_from_json_file(filename):
-    """Creates a Python object from a given JSON file"""
-    with open(filename) as f:
-        return json.load(f)
+    """
+    Function that creates an Object from a “JSON file”
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
