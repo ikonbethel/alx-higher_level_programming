2c7bf2b50058ab8ae35eeb1723afdd5a432373f5
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2024-02-21 02:32:29 +0100

    Add files via upload

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Script that adds all arguments to a Python list, and then saves them to a file
+"""
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+filename = "add_item.json"
+
+try:
+    arguments = load_from_json_file(filename)
+except Exception:
+    arguments = []
+
+for item in sys.argv[1:]:
+    arguments.append(str(item))
+
+save_to_json_file(arguments, filename)
