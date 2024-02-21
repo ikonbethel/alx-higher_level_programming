79d1003d0fb26838e8fa0faeff7a8e5060e187f4
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2024-02-21 02:53:11 +0100

    Update 5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-"""This module defines a JSON file-writing function"""
+"""
+Module containing function that writes an Object
+to a text file, using a JSON representation.
+"""
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """Writes an object to a text file using JSON format"""
-    with open(filename, "w") as f:
-        json.dump(my_obj, f)
+    """
+    Function that writes an Object to a text file, using a JSON representation.
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(my_obj, file)
