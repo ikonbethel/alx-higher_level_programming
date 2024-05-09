ccfd037c2d7d66981bb0adfa023b537dd24a07e3
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2024-05-09 06:54:29 -0700

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# display all HTTP methods the server will accept using curl..
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+# takes in a URL and displays all HTTP methods the server will accept
+curl -sI "$1" | grep "Allow:" | sed -ne 's/^Allow: //p'
