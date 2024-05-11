a92c8503d69829dd683a13b25ffa540e84f1be43
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2024-05-10 22:00:59 -0700

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-# Display the status.
-import requests
-import sys
+'''
+Sends a request to the URL and displays the value
+of the variable X-Request-Id in the response header.
+'''
 
-if __name__ == "__main__":
 
-    result = requests.get(sys.argv[1])
-    print(result.headers.get('X-Request-Id'))
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    url = argv[1]
+    content = requests.get(url)
+    request_id = content.headers.get('X-Request-Id')
+    print(request_id)
