24d8401b2f1aaa135f217c083a499f147702692c
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2024-05-10 22:05:03 -0700

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-# Star wars API.
-import requests
+"""Takes your GitHub credentials (username and password)
+- uses the GitHub API to display your id.
+"""
 import sys
+import requests
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-
-    usr = sys.argv[1]
-    pwd = sys.argv[2]
-    value = {usr: pwd}
-    result = requests.get("https://api.github.com/user",
-                          auth=requests.auth.HTTPBasicAuth(usr, pwd))
-
-    if result.status_code < 400:
-        dict_data = result.json()
-        print(dict_data['id'])
-    else:
-        print(None)
+    url = "https://api.github.com/user"
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get(url, auth=auth)
+    print(r.json().get("id"))
