209c51815ed7d98d044411507ca85c0833bf5f79
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2024-05-10 22:00:20 -0700

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-# Display the status.
-import requests
+''' fetches https://alx-intranet.hbtn.io/status using requests'''
 
-if __name__ == "__main__":
 
-    result = requests.get('https://intranet.hbtn.io/status')
-    r = result.text
+if __name__ == '__main__':
+    import requests
+    url = 'https://alx-intranet.hbtn.io/status'
+    content = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(r)))
-    print("\t- content: {}".format(r))
+    print("\t- type: {}".format(type(content.text)))
+    print("\t- content: {}".format(content.text))
